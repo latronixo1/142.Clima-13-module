41c6a865274b874cc2ce666249a0d860edd27a19
latronixo <pamerra@mail.ru>
2025-02-20 01:23:53 +0300

    Initial Commit

---
+++ 142.Clima-13-module/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  142.Clima-13-module
+//
+//  Created by Валентин Картошкин on 20.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
