f51ee78e61300ddb1e6b1eaa26c0f10b1d2279f2
latronixo <pamerra@mail.ru>
2025-02-20 15:52:47 +0300

    10:14, Assets, LaunchScreen, не готово

---
+++ 142.Clima-13-module/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  142.Clima-13-module
+//
+//  Created by Валентин Картошкин on 20.02.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let background = "background"
+    static let weatherColor = "weatherColor"
+    static let celsius = "°"
+    static let searchSF = "magnifyingglass" //перевод magnifyingglass - увеличить в стекле
+    static let geoSF = "location.circle.fill"
+    static let search = "Search"
+    static let condition = "cloud.rain"
+}
--- 142.Clima-13-module/ViewController.swift
+++ 142.Clima-13-module/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    private lazy var mainStackView: UIStackView = {
+        let element = UIStackView()
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var backgroundImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: Constants.background)    //отсылка на файл Constants.swift, который есть у нас в проекте, там в этой переменной есть имя файла
+        element.contentMode = .scaleAspectFill
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setViews()
+        setupConstaints()
         
+        view.backgroundColor = .green
     }
-
+    
+    private func setViews() {
+        view.addSubview(backgroundImageView)
+    }
+ 
 
 }
 
+extension ViewController {
+    private func setupConstaints() {
+
+        backgroundImageView.snp.makeConstraints { make in   //make - это то, что написано в начале строки (в данном случае это backgroundImageView
+            make.top.leading.trailing.bottom.equalToSuperview()
+//          make.edges.equalToSuperview()
+            
+        }
+
+        
+        NSLayoutConstraint.activate([
+//            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+//            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+        ])
+    }
+}
